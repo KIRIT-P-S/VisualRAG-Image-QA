c3b6061d98c877832b177b05e062f1a0e1decc4e
KIRIT-P-S <pskirit7545@gmail.com>
2026-07-23 09:52:46 +0530

    check if poppler is pre-installed on render

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-apt-get install -y poppler-utils
+which pdftoppm || echo "poppler not found"
 pip install -r requirements.txt
 python -c "from transformers import CLIPModel, CLIPProcessor; CLIPModel.from_pretrained('openai/clip-vit-base-patch32'); CLIPProcessor.from_pretrained('openai/clip-vit-base-patch32')"
